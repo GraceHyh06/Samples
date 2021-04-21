9f5422e196e6d0d02851fb785d1e229b9dc5231d
Grace.He <grace.he@printpal.com>
2021-04-21 18:46:20 +0800

    Add Topshelf Service Sample

---
+++ Samples.TopshelfService/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Topshelf;
+
+namespace Samples.TopshelfService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>{
+                x.Service<SampleService>(s => {
+                    s.ConstructUsing(() => new SampleService());
+                    s.WhenStarted(p => p.Start());
+                    s.WhenStopped(p => p.Stop());
+                });
+            });
+        }
+    }
+}
---
+++ Samples.TopshelfService/SampleService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Samples.TopshelfService
+{
+    public class SampleService
+    {
+        public void Start() {
+            Console.WriteLine("Service start...");
+        }
+
+        public void Stop() {
+            Console.WriteLine("Service stop.");
+        }
+    }
+}
